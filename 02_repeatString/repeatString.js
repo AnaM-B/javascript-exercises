5f732e5e7daf6a84a13fe20a55b82ab4dd413df6
AnaM-B <109470816+AnaM-B@users.noreply.github.com>
2023-11-11 20:31:16 +0000

    Created a for loop that concatenates the user's string to a new string a pre-specified number of times. The repetitions must be more than 0. If 0, an error is returned.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
+const repeatString = function(userString, repetitions) {
+    if (repetitions < 0) return "ERROR";
 
+    let newString = "";
+
+    for (let i = 0; i < repetitions; i++) {
+        newString += userString;
+    }
+
+    return newString
 };
 
 // Do not edit below this line
