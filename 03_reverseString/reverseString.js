77bed14ab6553bbccbc87afdc69cf97482c02268
AnaM-B <109470816+AnaM-B@users.noreply.github.com>
2023-11-11 20:54:21 +0000

    Wrote a reverseString function that reverses the characters of a string by splitting it into an array, reversing the array, then joining array elements back into a string. In a comment is a different solution using a for loop.

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,4 +1,11 @@
-const reverseString = function() {
+const reverseString = function(string) {
+    let newString = string.split("").reverse().join("");
+
+    // for (let i = string.length; i > 0; i--) {
+    //    newString += string[i - 1];
+    // }
+
+    return newString;
 
 };
 
