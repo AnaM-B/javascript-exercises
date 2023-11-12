c030a78f7616f0228ab1ce8117aa70729beb086c
AnaM-B <109470816+AnaM-B@users.noreply.github.com>
2023-11-12 17:44:38 +0000

    Implemented a solution which removes unwanted items from the original array.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,16 @@
-const removeFromArray = function() {
+const removeFromArray = function(array) {
+
+    for (let i = 1; i < arguments.length; i++) {
+        if (array.includes(arguments[i])) {
+            let unwantedItemIndex = array.indexOf(arguments[i]);
+            array.splice(unwantedItemIndex, 1)
+        }
+    }
+
+    return array;
 
 };
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
