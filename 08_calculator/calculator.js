f65d3a8f8432c2780dc646def06adf02c402bf18
AnaM-B <109470816+AnaM-B@users.noreply.github.com>
2023-12-19 12:38:37 +0000

    Addition and subtraction functions are simple and look ok. Will take another look at the rest of the functions to try to polish them.

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,48 @@
-const add = function() {
-	
+const add = function(x, y) {
+	return x + y;
 };
 
-const subtract = function() {
-	
+const subtract = function(x, y) {
+	return x - y;
 };
 
 const sum = function() {
-	
+  let sumResult = 0;
+
+	for (let i = 0; i < arguments[0].length; i++) {
+    sumResult += arguments[0][i];
+  }
+
+  return sumResult;
 };
 
 const multiply = function() {
+  let product = 1;
 
+  for (let i = 0; i < arguments[0].length; i++) {
+    product *= arguments[0][i];
+  }
+
+  return product;
 };
 
-const power = function() {
-	
+const power = function(x, y) {
+  return Math.pow(x, y);
 };
 
-const factorial = function() {
-	
+const factorial = function(x) {
+	let factArray = [];
+  let product = 1;
+
+  for(let i = 0; i < x; i++) {
+    factArray.push(x - i);
+  }
+
+  for (let i = 0; i < factArray.length; i++) {
+    product *= factArray[i];
+  }
+
+  return product;
 };
 
 // Do not edit below this line
