650c945f45c6833b11a87bae1cff4e019aefa30e
AnaM-B <109470816+AnaM-B@users.noreply.github.com>
2024-01-05 16:47:59 +0000

    Implemented a solution which uses an array and a for loop to calculate fibb numbers until the desired index has been reached.

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
-
+const fibonacci = function(index) {
+    if (index < 0) return "OOPS";
+    
+    let arr = [0, 1];
+  
+    for (let i = 0; arr.length <= index; i++) {
+        arr.push(arr[i] + arr[i + 1])
+    };
+  
+    return arr[index];
 };
 
 // Do not edit below this line
