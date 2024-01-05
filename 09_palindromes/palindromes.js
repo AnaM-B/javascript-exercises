6a71c6fe28b13db978fcfa1c16ebe9f2cda2c7ec
AnaM-B <109470816+AnaM-B@users.noreply.github.com>
2024-01-05 16:15:06 +0000

    Specifically wanted to write a solution that does not use regex.

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,19 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    str = str.toLowerCase();
+    let strArray = [];
 
+    for (let i = 0; i < str.length; i++) {
+        if ((str.at(i) >= "a" && str.at(i) <= "z") || 
+        (str.at(i) >= 1 && str.at(i) <= 9)) {
+            
+            strArray.push(str.at(i));
+        }
+    }
+
+    return strArray.join("") == strArray.reverse().join("");
 };
+
+palindromes('rac3e3car!');
 
 // Do not edit below this line
 module.exports = palindromes;
