48cdc83726a46cf526b3e4be8677a1e70a4fa97b
AnaM-B <109470816+AnaM-B@users.noreply.github.com>
2024-01-05 17:10:42 +0000

    Wrote two solutions, one using the forEach method and another using map.

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,13 @@
-const getTheTitles = function() {
+const getTheTitles = function(books) {
+    return books.map((element) => element.title);
+    
+    /* First solution using forEach method
+    const titles = [];
 
+    books.forEach(element => titles.push(element.title));
+
+    return titles;
+    */
 };
 
 // Do not edit below this line
